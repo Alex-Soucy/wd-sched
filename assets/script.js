52ba33c6bb29c1c7254a274f2dafa24df9acf7a9
Alex Soucy <asoucy22@yahoo.com>
2021-11-07 21:03:55 -0500

    added time blocks in index.html and save button functionality in script.js

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,16 @@
 // show current day, date, and time
 var todaysDate = moment().format('dddd, MMM do YYYY, h:mm:ss a');
 $("#currentDay").html(todaysDate);
+
+// add functionality for save button and save time and task to local storage
+$(document).ready(function () {
+    $(".saveBtn").on("click", function () {
+        
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        localStorage.setItem(time, text);
+    })
+
+
+})
